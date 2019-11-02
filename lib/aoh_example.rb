6a37ecab70b4cd91c9fadc291258e69e8fcd6cf6
Robert M Ricci <rmricci121@gmail.com>
2019-11-02 10:40:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -4,7 +4,7 @@ DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Kate Gompert", occupation: "None" }
+BRUCE_G = {name:"Bruce Green", occupation:"Fan of Mildred"}
 
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
@@ -15,7 +15,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  lit =[ name:  "Don Gately", occupation: "Live-in Staff" },{ name:  "Joelle van Dyne", occupation: "Radio Perso"},{ name:  "Pat Monteseian", occupation: "Staff" },{ name:  "Kate Gompert", occupation: "None" },]
+  lit =[ name:  "Don Gately", occupation: "Live-in Staff" },{ name:  "Joelle van Dyne", occupation: "Radio Perso"},{ name:  "Pat Monteseian", occupation: "Staff" },{ name:  "Kate Gompert", occupation: "None" },{name:"Bruce Green", occupation:"Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
